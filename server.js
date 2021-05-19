9bef97709afb79311bbd41cd5787dfdc1ac2b7bf
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-19 12:28:30 -0500

    Deleted node_modules. Added .gitignore.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 
-mongoose.connect(process.env,MONGODB_URI || "mongodb://localhost/<name-goes-here>", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/<name-goes-here>", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
