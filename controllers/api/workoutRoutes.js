a6e0c9074032f393a1514dc7408b5eae16c049f1
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-19 12:39:08 -0500

    Added Workout model. Updated workoutRoutes.js.

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,33 +1,30 @@
 const router = require('express').Router();
-
-// const databaseUrl = "<your-database-name>";
-// const collections = [];
-
-// const db = mongojs(databseUrl, coollections);
+const Workout = require("../../models/Workout");
 
 router.get('/', (req, res) => {
-  db.workouts.find({}, (error, data) => {
-    error ? console.error(error) : res.status(200).json(data);
-  });
+  Workout.find({})
+    .then(data => res.status(200).json(data))
+    .catch(error => console.error(error));
 });
 
 router.put('/:id', (req, res) => {
   console.log(req.body);
-  // db.workouts.update({ _id: mongojs.ObjectId(req.params.id) }, req.body, (error, data) => {
-  //   error ? console.error(error) : res.status(200).json(data);
-  // });
+  Workout.update({ _id: mongojs.ObjectId(req.params.id)}, req.body, (error, data) => {
+    error ? console.error(error) : res.status(200).json(data);
+  });
 });
 
 router.post('/', ({ body }, res) => {
 
   console.log(body);
-  // db.workouts.insert(body, (error, data) => {
-  //   error ? console.error(error) : res.status(200).json(data);
-  // });
+  Workout.create(body)
+    .then(data => res.status(200).json(data))
+    .catch(error => console.error(error))
+
 });
 
 router.get('/range', (req, res) => {
-  db.workouts.find({}, (req, res) => {
+  Workout.find({}, (req, res) => {
     error ? console.error(error) : res.status(200).json(data);
   });
 });
